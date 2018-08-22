19814a6b62825afc2296f9086a4492aa6424c09d
martindrech <martindrech@df.uba.ar>
2018-08-22 18:05:06 -0300

    update of play

--- play.py
+++ play.py
@@ -1,33 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 22 15:36:04 2018
-
-@author: martindrech
-"""
-
 import sounddevice as sd
 import numpy as np
 import pylab as plt
+import time
 
-f = 440
-D = 2
-fs = 44000
-
-
-m = D*f
-n = int(fs/f)
-N = n*m
-
-
-
-x = np.linspace(0, 2*np.pi*m, N)
-
-
-data = np.sin(2*np.pi*f*x)
-sd.default.samplerate = fs
+def playTone(f, D):
+    #f = 220
+    #D = 5
+    fs = 44000
+    
+    
+    m = D*f
+    n = int(fs/f)
+    N = n*m
+    
+    
+    
+    x = np.linspace(0, D, N)
+    
+    
+    data = np.sin(2*np.pi*f*x)+np.sin(2*np.pi*f*1.5*x)+np.sin(2*np.pi*f*x*2)
+    sd.default.samplerate = fs
+    #time.sleep(D)
+    sd.play(data)
+    
 #%%
-sd.play(data)
-
+tones = np.array([165, 175, 196, 220, 247, 262, 294, 330]*2)*2
+for i in tones:
+    playTone(i, .2)
+    print(i)
 #%%
-plt.plot(x, data)
+playTone(262, 2)
