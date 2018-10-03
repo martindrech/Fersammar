631adb8824af7bf6577d9c3a6eb39ce79c1a3594
Samantha Kucher <samanthakucher@gmail.com>
2018-10-03 19:07:29 -0300

    Hacemos funcion para medir

--- Placa_de_adquisicion/tomar_medicion.py
+++ Placa_de_adquisicion/tomar_medicion.py
@@ -7,10 +7,12 @@ Este es un archivo temporal.
 
 import nidaqmx
 import time
+import numpy as np
 from matplotlib import pyplot as plt
 import nidaqmx.system
 system = nidaqmx.system.System.local()
 system.driver_version
+import sounddevice as sd
 
 for device in system.devices:
     print(device)
@@ -24,16 +26,41 @@ task = nidaqmx.Task()
 task.ai_channels.add_ai_voltage_chan('Dev1/ai0')
 #%%
 medicion = []
+#tiempo = []
+start_time =time.time()
 while True:
     data = task.read()
-    print(data)
+    #print(data)
     medicion.append(data)
-    time.sleep(0.2)
-    if len(medicion)>100:
+    #tiempo.append(time.time())
+    #time.sleep(0.2)
+    if (time.time()-start_time)>15:
         break
-
-#%%
 
 plt.plot(medicion)
 
-#ch = task._ai_channels.add_ai_voltage_chan()
+#%%
+
+
+from Placa_de_audio.play import generador_de_senhal, playrec_tone, play_tone
+
+def barrido_frecuencias(tiempo, f0, fN, df):
+    medicion_salida = []
+    frecuencias = np.arange(f0, fN, df)
+    for f in frecuencias:
+        play_tone(f, tiempo, amplitud=1)
+        data = task.read()
+        transformada = np.fft.fft(data)
+        indice_max_frecuencia = np.argmax(transformada)
+        #print(data)
+        medicion_salida.append(indice_max_frecuencia)
+        #tiempo.append(time.time())
+        #time.sleep(0.2)
+    return medicion_salida
+
+        
+indice_frecuencia_salida = barrido_frecuencias(2,100,10000,100)
+frecuencia_entrada = np.arange(100,10000,100)
+freqs = np.fft.fftfreq(x.size)*2*np.pi/dt #obtener el dt
+
+#Tambien se podria mandar a medir todo junto y despues analizar, para que vaya mas rapido
